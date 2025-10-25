483a3fb4356155059c1f1d964c401d30e62f9ac4
Quozul <30729291+Quozul@users.noreply.github.com>
2025-10-25 17:45:46 +0200

    Pure NodeJS HTTP server and unit test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-export function add(a: number, b: number): number {
-	return a + b;
+import {
+	createServer,
+	type IncomingMessage,
+	type ServerResponse,
+} from "node:http";
+
+export function handleRequest(req: IncomingMessage, res: ServerResponse) {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+
+	const body = `${req.method} ${req.url}\n`;
+	res.writeHead(200, {
+		"Content-Type": "text/plain; charset=utf-8",
+		"Content-Length": Buffer.byteLength(body),
+	});
+	res.end(body);
+}
+
+if (import.meta.main) {
+	const PORT = process.env.PORT || 3000;
+
+	const server = createServer(handleRequest);
+	server.listen(PORT, () => {
+		const address =
+			typeof server.address() === "object"
+				? `http://127.0.0.1:${PORT}`
+				: server.address;
+		console.log("🌐 Server listening on", address);
+	});
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,25 @@
 import assert from "node:assert";
-import { test } from "node:test";
-import { add } from "../src/index.ts";
+import type { IncomingMessage, ServerResponse } from "node:http";
+import { mock, test } from "node:test";
+import { handleRequest } from "../src/index.ts";
 
-test("add", (t) => {
-	t.test("should add two numbers", () => {
-		assert.strictEqual(add(5, 2), 7);
+test("handleRequest", (t) => {
+	t.test("should handle root path", () => {
+		// Arrange
+		const req = {
+			method: "GET",
+			url: "/",
+		} as IncomingMessage;
+		const endMock = mock.fn();
+		const res = {
+			writeHead: mock.fn(),
+			end: endMock,
+		} as unknown as ServerResponse;
+
+		// Act
+		handleRequest(req, res);
+
+		// Assert
+		assert.strictEqual(endMock.mock.calls[0].arguments[0], "GET /\n");
 	});
 });
